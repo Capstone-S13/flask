e8f2c83d5f6cdb5b780b5e57cd20e10bbd5afbcb
Yi Xian <yyx543@users.noreply.github.com>
2022-05-18 13:52:00 +0800

    test run

--- app.py
+++ app.py
@@ -2,22 +2,27 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from uuid import uuid4
+import system
 
+# Account Type
 CUSTOMER = 0
 STORE = 1
 
+# Customer Landing Tab
 CUSTOMER_SHOPS = 0
 CUSTOMER_DELIVERY = 1
 
+# Store Landing Tab
 STORE_INCOMING = 0
 STORE_PREPARING = 1
 STORE_DELIVERY = 2
 
+# Order Statuses
 ORDER_RECEIVED = 0
 ROBOT_DISPATCHED = 1
 AT_STORE_HUB = 2
 BETWEEN_HUBS = 3
-AT_LOCAL_HUB = 4
+AT_DEST_HUB = 4
 ARRIVED = 5
 DELIVERED = 6
 CANCELLED = 7
@@ -26,36 +31,38 @@ FAILED = 8
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-# dbBind = {'order': 'sqlite:///order.db',
-#           'ingress': 'sqlite:///ingress.db'}
-# app.config['SQLALCHEMY_BINDS'] = dbBind
+dbBind = {'order': 'sqlite:///order.db',
+          'ingress': 'sqlite:///ingress.db'}
+app.config['SQLALCHEMY_BINDS'] = dbBind
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class UserDb(db.Model):
-    userId = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.String(50), primary_key=True)
     email = db.Column(db.String(200), primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    postalCode = db.Column(db.Integer, nullable=False)
+    buildingName = db.Column(db.String(50), nullable=False)
     unitNumber = db.Column(db.String(200), nullable=False)
     accountType = db.Column(db.Integer, nullable=False)
     
 class OrderDb(db.Model):
     __bind_key__ = 'order'
-    orderId = db.Column(db.Integer, primary_key=True)
-    customerId = db.Column(db.Integer, nullable=False)
-    storeId = db.Column(db.Integer, nullable=False)
+    orderId = db.Column(db.String(50), primary_key=True)
+    customerId = db.Column(db.String(50), nullable=False)
+    storeId = db.Column(db.String(50), nullable=False)
     orderDetails = db.Column(db.String(1000), nullable=False) # do we need this since we just doing 1 item per store
     status = db.Column(db.Integer, nullable=False)
-    robotID = db.Column(db.Integer)
+    robotID = db.Column(db.String(50))
 
 class IngressDb(db.Model):
     __bind_key__ = 'ingress'
     postalCode = db.Column(db.Integer, primary_key=True)
     ingressPoint = db.Column(db.String(200), nullable=False)
     
-@app.route('/login', methods = ['GET', 'POST'])
+# Login Page
+@app.route('/', methods = ['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -72,17 +79,18 @@ def login():
                 return redirect('/store/{}/landing/{}'.format(userAcc.userId, STORE_INCOMING))
     return render_template('login.html', error=error)
 
-@app.route('/customer/<int:userId>/landing/<int:landingTab>', methods=['POST','GET'])
-#customer landing page
-def customerLandingPage(userId, landingTab):    
-    browse = UserDb.filter_by(accountType=STORE)
-    orders = OrderDb.filter_by(customerId=userId)
+# Customer Landing Page
+@app.route('/customer/<string:userId>/landing/<int:landingTab>', methods=['GET'])
+def customerLandingPage(userId, landingTab):
+    browse = UserDb.filter_by(accountType=STORE) # system.get_all_stores()
+    orders = OrderDb.filter_by(customerId=userId) # system.get_orders(userId)
     return render_template("customerLanding.html", browse=browse, orders=orders, landingTab=landingTab)
 
-@app.route('/customer/<int:userId>/create/<int:storeId>')
-# creating order
+# Customer Creating Order
+@app.route('/customer/<string:userId>/create/<string:storeId>')
 def create(userId, storeId):
-    orderId = uuid4().int
+    # system.create_order(userId, storeId)
+    orderId = uuid4()
     new_order = OrderDb(orderId=orderId, customerId=userId, storeId=storeId, status=ORDER_RECEIVED)
     
     try:
@@ -92,49 +100,50 @@ def create(userId, storeId):
     except:
         return "There was an error creating the order"
 
-@app.route('/customer/<int:userId>/order/<int:orderId>', methods=['POST', 'GET'])
-# viewing single order page
-def order(userId, orderId):
-    order = OrderDb.query.get_or_404(orderId)
-    return render_template("order.html", order=order, userId=userId)
+# Customer Viewing Single Order
+@app.route('/customer/<string:userId>/order/<string:orderId>', methods=['POST', 'GET'])
+# def order(userId, orderId):
+#     order = OrderDb.query.get_or_404(orderId)
+#     return render_template("order.html", order=order, userId=userId)
+
+# Customer Delete Order
+@app.route('/customer/<string:userId>/order/<string:orderId>/delete')
+# def delete(userId, orderId):
+#     order_to_delete = OrderDb.query.get_or_404(orderId)
     
-@app.route('/customer/<int:userId>/order/<int:orderId>/delete')
-# delete order
-def delete(userId, orderId):
-    order_to_delete = OrderDb.query.get_or_404(orderId)
-    
-    if order_to_delete.customerId == userId:
-        try:
-            OrderDb.session.delete(order_to_delete)
-            OrderDb.session.commit()
-            return redirect("/customer/{}/landing/{}".format(userId, CUSTOMER_DELIVERY))
-        except:
-            return "There was an error deleting the order"
-    else:
-        return "Order does not belong to user"
+#     if order_to_delete.customerId == userId:
+#         try:
+#             OrderDb.session.delete(order_to_delete)
+#             OrderDb.session.commit()
+#             return redirect("/customer/{}/landing/{}".format(userId, CUSTOMER_DELIVERY))
+#         except:
+#             return "There was an error deleting the order"
+#     else:
+#         return "Order does not belong to user"
 
-@app.route('/customer/<int:userId>/order/<int:orderId>/robotId')
-# retrieve order's robotId
+# Customer Retrieve Order RobotId (refresh button?)
+@app.route('/customer/<string:userId>/order/<string:orderId>/robotId')
 
-@app.route('/customer/<int:userId>/order/<int:orderId>/status')
-# retrieve order status
+# Customer Retrieve Order Status (refresh button?)
+@app.route('/customer/<string:userId>/order/<string:orderId>/status')
 
 
 
-@app.route('/store/<int:userId>/landing/<int:landingTab>')
-# store landing page
+# Store Landing Page
+@app.route('/store/<string:userId>/landing/<int:landingTab>')
 def storeLandingPage(userId, landingTab):    
     orders = OrderDb.filter_by(storeId=userId)
     return render_template("storeLanding.html", orders=orders, landingTab=landingTab)
 
-@app.route('/store/<int:userId>/order/<int:orderId>', methods=['POST', 'GET'])
-# viewing single order page
+# Store Viewing Single Order Page
+@app.route('/store/<string:userId>/order/<string:orderId>', methods=['POST', 'GET'])
 def order(userId, orderId):
     order = OrderDb.query.get_or_404(orderId)
     return render_template("order.html", order=order, userId=userId)
+########################### Create Order HTML #########################################
 
-@app.route('/store/<int:userId>/order/<int:orderId>/delete')
-# reject order
+# Store Reject Order
+@app.route('/store/<string:userId>/order/<string:orderId>/delete')
 def delete(userId, orderId):
     order_to_delete = OrderDb.query.get_or_404(orderId)
     
@@ -148,8 +157,8 @@ def delete(userId, orderId):
     else:
         return "Order does not belong to user"
 
-@app.route('/store/<int:userId>/order/<int:orderId>/<int:status>')
-# update order (status)
+# Update Order (Status)
+@app.route('/store/<string:userId>/order/<string:orderId>/<int:status>')
 def set_status(userId, orderId, status):
     order_to_update = OrderDb.query.get_or_404(orderId)
     order_to_update.status = status
